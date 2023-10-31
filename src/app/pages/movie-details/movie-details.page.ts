d5ae6c56abb20d3f557f4abbb2f33a073b9e97c8
busraurek <busraurekk8454@gmail.com>
2023-10-31 17:39:52 +0300

    movieApp yuklendi

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit {
+  // movie = null;
+  movie: any | null = null;
+  imageBaseUrl = environment.images;
+
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id') || '';
+    this.movieService.getMovieDetails(id).subscribe((res: any) => {
+      console.log(res);
+      this.movie = res;
+    });
+  }
+
+  openHomePage() {
+    window.open(this.movie.homepage);
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  images: 'https://image.tmdb.org/t/p',
+  apiKey: '7055336680570374e6a490946a6b7f25',
+  baseUrl: 'https://api.themoviedb.org/3',
 };
